a770f68acc69be2dfaf16a00ffad812718a4dac7
onlydel <onlydel@realgrid.com>
2020-06-30 17:21:00 +0900

    테스트 추가 - 달력 편집기

---
+++ stories/07-editor/onlydel.calendar.stories.js
@@ -0,0 +1,22 @@
+import { useEffect } from "@storybook/client-api";
+import { gridContainer, init, renderMessage } from "../_common";
+
+export default {
+  title: "07-편집기",
+};
+
+export const 달력팝업 = () => {
+  useEffect(() => {
+    const { gridView } = init("realgrid");
+
+    setTimeout(() => {
+      gridView.setCurrent({
+        dataRow: 1,
+        column: "StartDate",
+      });
+      gridView.showEditor({ dropdown: true });
+    }, 200);
+  });
+
+  return gridContainer();
+};
