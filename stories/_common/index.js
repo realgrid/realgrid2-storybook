7fb00b58c1213ec6ca962c9ab87e69c3dc98f7f3
onlydel <onlydel@realgrid.com>
2020-07-07 15:35:01 +0900

    init 함수 추가/수정

--- stories/_common/index.js
+++ stories/_common/index.js
@@ -1,3 +1,9 @@
-export { init } from './init';
-export { columns01 } from './columns01';
-export { createButton, renderMessage, gridContainer, withContainer } from './utils';
+export { init0, init1, init2, init } from "./init";
+export { fields01 } from "./fields01";
+export { columns01 } from "./columns01";
+export {
+  createButton,
+  renderMessage,
+  gridContainer,
+  withContainer,
+} from "./utils";
