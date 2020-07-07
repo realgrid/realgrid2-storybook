8f9be740bf4479e75a4173bff65cad235575aa74
onlydel <onlydel@realgrid.com>
2020-07-07 15:25:07 +0900

    Provider, View 생성 관련 테스트 추가

--- stories/01-Init/onlydel.stories.js
+++ stories/01-Init/onlydel.stories.js
@@ -1,9 +1,9 @@
-import { useEffect } from '@storybook/client-api';
-import { renderMessage } from '../_common';
-import './style.css';
+import { useEffect } from "@storybook/client-api";
+import { renderMessage } from "../_common";
+import "./style.css";
 
 export default {
-  title: '01-빈그리드'
+  title: "01-빈그리드",
 };
 
 const container = `
@@ -14,7 +14,7 @@ const container = `
 export const 빈그리드 = () => {
   useEffect(() => {
     const dataProvider = new RealGrid.LocalDataProvider();
-    const gridView = new RealGrid.GridView('realgrid');
+    const gridView = new RealGrid.GridView("realgrid");
     gridView.setDataSource(dataProvider);
 
     renderMessage(RealGrid.getVersion());
@@ -24,14 +24,62 @@ export const 빈그리드 = () => {
 };
 빈그리드.story = {
   parameters: {
-    notes: '이 화면은 아무것도 없는 빈 그리드 입니다.',
+    notes: "이 화면은 아무것도 없는 빈 그리드 입니다.",
   },
+};
+
+/**
+ * provider만 생성
+ */
+export const provider만_생성 = () => {
+  useEffect(() => {
+    const dataProvider = new RealGrid.LocalDataProvider();
+  });
+
+  return container;
+};
+
+/**
+ * view만 생성
+ */
+export const View만_생성 = () => {
+  useEffect(() => {
+    const gridView = new RealGrid.GridView("realgrid");
+  });
+
+  return container;
+};
+
+/**
+ * provider, view 생성
+ */
+export const provider_view_생성 = () => {
+  useEffect(() => {
+    const dataProvider = new RealGrid.LocalDataProvider();
+    const gridView = new RealGrid.GridView("realgrid");
+  });
+
+  return container;
+};
+
+/**
+ * provider, view 생성 후 연결
+ */
+export const provider_view_생성후_연결 = () => {
+  useEffect(() => {
+    const dataProvider = new RealGrid.LocalDataProvider();
+    const gridView = new RealGrid.GridView("realgrid");
+    gridView.setDataSource(dataProvider);
+  });
+
+  return container;
 };
 
 export const 빈그리드_메시지 = () => {
   useEffect(() => {
     const dataProvider = new RealGrid.LocalDataProvider();
-    const gridView = new RealGrid.GridView('realgrid');
+    const gridView = new RealGrid.GridView("realgrid");
+    gridView.displayOptions.showEmptyMessage = true;
     gridView.displayOptions.emptyMessage = "표시할 데이타가 없습니다.";
     gridView.setDataSource(dataProvider);
   });
@@ -40,14 +88,14 @@ export const 빈그리드_메시지 = () => {
 };
 빈그리드_메시지.story = {
   parameters: {
-    notes: '빈 그리드에 데이터 없음 메시지를 변경한 화면',
+    notes: "빈 그리드에 데이터 없음 메시지를 변경한 화면",
   },
 };
 
 export const 빈그리드_헤더높이40 = () => {
   useEffect(() => {
     const dataProvider = new RealGrid.LocalDataProvider();
-    const gridView = new RealGrid.GridView('realgrid');
+    const gridView = new RealGrid.GridView("realgrid");
     gridView.setDataSource(dataProvider);
 
     gridView.header.height = 40;
@@ -57,14 +105,14 @@ export const 빈그리드_헤더높이40 = () => {
 };
 빈그리드_헤더높이40.story = {
   parameters: {
-    notes: '빈 그리드에 헤더 높이를 40 픽셀로 조정한 화면',
+    notes: "빈 그리드에 헤더 높이를 40 픽셀로 조정한 화면",
   },
 };
 
 export const 빈그리드_헤더감추기 = () => {
   useEffect(() => {
     const dataProvider = new RealGrid.LocalDataProvider();
-    const gridView = new RealGrid.GridView('realgrid');
+    const gridView = new RealGrid.GridView("realgrid");
     gridView.setDataSource(dataProvider);
 
     gridView.header.visible = false;
@@ -76,7 +124,7 @@ export const 빈그리드_헤더감추기 = () => {
 export const 빈그리드_푸터높이40 = () => {
   useEffect(() => {
     const dataProvider = new RealGrid.LocalDataProvider();
-    const gridView = new RealGrid.GridView('realgrid');
+    const gridView = new RealGrid.GridView("realgrid");
     gridView.setDataSource(dataProvider);
 
     gridView.footer.height = 40;
@@ -86,14 +134,14 @@ export const 빈그리드_푸터높이40 = () => {
 };
 빈그리드_푸터높이40.story = {
   parameters: {
-    notes: '빈 그리드에 푸터의 높이를 40 픽셀로 조정한 화면',
+    notes: "빈 그리드에 푸터의 높이를 40 픽셀로 조정한 화면",
   },
 };
 
 export const 빈그리드_푸터감추기 = () => {
   useEffect(() => {
     const dataProvider = new RealGrid.LocalDataProvider();
-    const gridView = new RealGrid.GridView('realgrid');
+    const gridView = new RealGrid.GridView("realgrid");
     gridView.setDataSource(dataProvider);
 
     gridView.footers.visible = false;
@@ -105,7 +153,7 @@ export const 빈그리드_푸터감추기 = () => {
 export const 빈그리드_인디케이터 = () => {
   useEffect(() => {
     const dataProvider = new RealGrid.LocalDataProvider();
-    const gridView = new RealGrid.GridView('realgrid');
+    const gridView = new RealGrid.GridView("realgrid");
     gridView.setDataSource(dataProvider);
 
     gridView.rowIndicator.visible = false;
@@ -115,14 +163,14 @@ export const 빈그리드_인디케이터 = () => {
 };
 빈그리드_인디케이터.story = {
   parameters: {
-    notes: '빈 그리드에 인디케이터 감추기',
+    notes: "빈 그리드에 인디케이터 감추기",
   },
 };
 
 export const 빈그리드_상태바 = () => {
   useEffect(() => {
     const dataProvider = new RealGrid.LocalDataProvider();
-    const gridView = new RealGrid.GridView('realgrid');
+    const gridView = new RealGrid.GridView("realgrid");
     gridView.setDataSource(dataProvider);
 
     gridView.stateBar.visible = false;
@@ -132,14 +180,14 @@ export const 빈그리드_상태바 = () => {
 };
 빈그리드_상태바.story = {
   parameters: {
-    notes: '빈 그리드에 상태바 감추기',
+    notes: "빈 그리드에 상태바 감추기",
   },
 };
 
 export const 빈그리드_체크바 = () => {
   useEffect(() => {
     const dataProvider = new RealGrid.LocalDataProvider();
-    const gridView = new RealGrid.GridView('realgrid');
+    const gridView = new RealGrid.GridView("realgrid");
     gridView.setDataSource(dataProvider);
 
     gridView.checkBar.visible = false;
@@ -149,6 +197,6 @@ export const 빈그리드_체크바 = () => {
 };
 빈그리드_체크바.story = {
   parameters: {
-    notes: '빈 그리드에 체크바 감추기',
+    notes: "빈 그리드에 체크바 감추기",
   },
 };
