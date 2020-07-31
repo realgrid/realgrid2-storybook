978bd671a136a457fe3cd5f5d75148d2829d12fd
tacrosst <tacrosst@realgrid.com>
2020-07-31 16:25:56 +0900

    상태바 케이스 추가

--- stories/11-stateBar/tacrosst.column.header.stories.js
+++ stories/11-stateBar/tacrosst.column.header.stories.js
@@ -16,3 +16,48 @@ export const 상태변경 = () => {
 
   return gridContainer();
 };
+
+export const 상태바숨김 = () => {
+  useEffect(() => {
+    const { dataProvider, gridView } = initDataset(1);
+    gridView.setStateBar({
+      visible: false
+    });
+  });
+
+  return gridContainer();
+};
+
+export const text로표시 = () => {
+  useEffect(() => {
+    const { dataProvider, gridView } = initDataset(1);
+    gridView.setStateBar({
+      mark: "text",
+      stateTexts: {
+        created: "C",
+        updated: "U",
+        deleted: "D",
+        createAndDeleted: "X"
+      }
+    });
+
+    dataProvider.setRowState(0, 'created');
+    dataProvider.setRowState(1, 'updated');
+    dataProvider.setRowState(2, 'createAndDeleted');
+    dataProvider.setRowState(3, 'deleted');
+  });
+
+  return gridContainer();
+};
+
+export const 상태바헤더푸터 = () => {
+  useEffect(() => {
+    const { dataProvider, gridView } = initDataset(1);
+    gridView.setStateBar({
+      headText: "H",
+      footText: "F"
+    });
+  });
+
+  return gridContainer();
+};
